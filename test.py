4ce9f048e573d7748e3bd6d95bc0ada29276afd8
Bryan Dixon <javawolfpack@gmail.com>
2020-09-01 10:46:15 -0700

    Basic Python Part 1

---
+++ test.py
@@ -0,0 +1,65 @@
+# x = 3
+# x = 3.0
+# x = True
+# x = "three"
+
+# def x(x):
+#     print(x)
+#     print(x) ##from stack overflow here: 
+
+x=5
+# if x < 3:
+#     print("True")
+# elif x < 5:
+#     print("Bob")
+# elif x < 7:
+#     print("Carl", end="\t")
+#     if x == 6:
+#         print("Jr")
+# else:
+#     print("Alice")
+
+# y = "7"
+# print(str(x)+"\t"+y)
+# print(x+int(y))
+
+a = [1,2]
+a.append(3)
+a += [4]
+a = [5] + a
+# for i in range(len(a)):
+#     print(i, end=" ")
+#     print(a[i])
+# print(list(range(0,len(a),2)))
+# i = 0
+# while i<len(a):
+#     print(a[i])
+#     i+=1
+
+# for el in a:
+#     print(el)
+
+# f = open("Dockerfile","r")
+# for line in f:
+#     if "code" in line or "WORKDIR" in line:
+#         stripped_line = line.rstrip()
+#         print(stripped_line)
+    
+# a += ["hi"]
+# a += [3.0]
+# a += [True]
+# print(a)
+def x():
+    print("bob")
+
+d = {True:x}
+d["key"] = "value"
+d["key"] += "value2"
+d["key"] = [1,2,3,4]
+d["key2"] = {"key3":"value"}
+
+d[True]()
+
+
+
+
