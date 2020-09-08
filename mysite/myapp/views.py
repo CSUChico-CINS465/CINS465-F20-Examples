f0b95289c09b64f1cc121b34e36c43707e929ef7
Bryan Dixon <javawolfpack@gmail.com>
2020-09-08 10:46:29 -0700

    Django Templates/Hello World/Pagination

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request, page=0):
+    title = "My Title"
+    content = list(range((page+1)*10))[page*10:page*10+10]
+    # seen = True
+    context = {
+        "title":title,
+        "body":content,
+        "seen":True,
+        "prev":page-1,
+        "next":page+1
+    }
+    return render(request, "index.html", context=context)
