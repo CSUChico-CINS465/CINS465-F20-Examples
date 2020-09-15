79b0ff1808fdbd034ea7c8a912e3987bb0e686a5
Bryan Dixon <javawolfpack@gmail.com>
2020-09-15 10:36:31 -0700

    Forms

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import models
+from . import forms
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        suggestion_form = forms.SuggestionForm(request.POST)
+        if suggestion_form.is_valid():
+            suggestion_form.save()
+            suggestion_form = forms.SuggestionForm()
+    else:
+        suggestion_form = forms.SuggestionForm()
     suggestions = models.SuggestionModel.objects.all()
     context = {
         "title":"Suggestions",
-        "suggestions":suggestions
+        "suggestions":suggestions,
+        "form":suggestion_form,
     }
     return render(request, "index.html", context=context)
 
