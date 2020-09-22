7f8b5b0593c027e8457b97f6a701224f5cea2273
Bryan Dixon <javawolfpack@gmail.com>
2020-09-22 10:46:39 -0700

    Auth/Login/Logout/Register

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
 
 from . import models
 from . import forms
 
+def logout_view(request):
+    logout(request)
+    return redirect("/login/")
+
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        suggestion_form = forms.SuggestionForm(request.POST)
-        if suggestion_form.is_valid():
-            suggestion_form.save(request)
-            suggestion_form = forms.SuggestionForm()
+        if request.user.is_authenticated:
+            suggestion_form = forms.SuggestionForm(request.POST)
+            if suggestion_form.is_valid():
+                suggestion_form.save(request)
+                suggestion_form = forms.SuggestionForm()
+        # else:
+        #     suggestion_form = forms.SuggestionForm()
     else:
         suggestion_form = forms.SuggestionForm()
     suggestion_objects = models.SuggestionModel.objects.all()
@@ -33,15 +44,49 @@ def index(request):
     }
     return render(request, "index.html", context=context)
 
+@login_required
 def page(request, page=0):
-    title = "My Title"
-    content = list(range((page+1)*10))[page*10:page*10+10]
-    # seen = True
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            suggestion_form = forms.SuggestionForm(request.POST)
+            if suggestion_form.is_valid():
+                suggestion_form.save(request)
+                suggestion_form = forms.SuggestionForm()
+        # else:
+        #     suggestion_form = forms.SuggestionForm()
+    else:
+        suggestion_form = forms.SuggestionForm()
+    suggestion_objects = models.SuggestionModel.objects.all()
+    suggestion_list = []
+    for sugg in suggestion_objects:
+        comment_objects = models.CommentModel.objects.filter(suggestion=sugg)
+        temp_sugg = {}
+        temp_sugg["suggestion"] = sugg.suggestion
+        temp_sugg["id"] = sugg.id
+        temp_sugg["author"] = sugg.author.username
+        temp_sugg["comments"] = comment_objects
+        suggestion_list+=[temp_sugg]
+
+    # comments = models.CommentModel.objects.all()
     context = {
-        "title":title,
-        "body":content,
-        "seen":True,
-        "prev":page-1,
-        "next":page+1
+        "title":"Suggestions",
+        "suggestions":suggestion_list,
+        "form":suggestion_form,
+        # "comments":comments
     }
-    return render(request, "page.html", context=context)
+    return render(request, "index.html", context=context)
+
+def register(request):
+    if request.method == "POST":
+        form_instance = forms.RegistrationForm(request.POST)
+        if form_instance.is_valid():
+            form_instance.save()
+            return redirect("/login/")
+    else:
+        form_instance = forms.RegistrationForm()
+    context = {
+        "form":form_instance,
+    }
+    return render(request, "registration/register.html", context=context)
